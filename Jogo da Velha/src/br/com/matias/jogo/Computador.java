b593ade88e2a07bdca5c28636eeaf9c9c92852b9
Manoel Matias <manoel13matias@gmail.com>
2020-06-26 22:12:51 -0300

    Adicionei funções.
    Adicionei o código para fazer a classe Computador funcionar.

--- Jogo da Velha/src/br/com/matias/jogo/Computador.java
+++ Jogo da Velha/src/br/com/matias/jogo/Computador.java
@@ -1,24 +1,62 @@
 package br.com.matias.jogo;
 
+import java.util.Random;
+
 /**
  *
  * @author Matias
  */
 public class Computador extends Jogador
 {
+    private Random aleatorio = new Random();
+
     public Computador(int jogador)
     {
         super(jogador);
+        this.jogador = jogador;
         System.out.println("Jogador 'Computador' criado!");
     }
     
     @Override
     public void jogar(Tabuleiro tabuleiro)
     {
+        tentativa(tabuleiro);
+        tabuleiro.setPosicao(tentativa, jogador);
     }
     
     @Override
     public void tentativa(Tabuleiro tabuleiro)
     {
+        do
+        {
+            do
+            {
+                System.out.print("Linha: ");
+                tentativa[0] = aleatorio.nextInt(3) + 1;
+                System.out.println(tentativa[0]);
+
+                if((tentativa[0] < 1) || (tentativa[0] > 3))
+                    System.out.println("Linha inválida. É 1, 2 ou 3!");
+            }
+            while((tentativa[0] < 1) || (tentativa[0] > 3));
+
+            do
+            {
+                System.out.print("Coluna: ");
+                tentativa[1] = aleatorio.nextInt(3) + 1;
+                System.out.println(tentativa[1]);
+
+                if((tentativa[1] < 1) || (tentativa[1] > 3))
+                    System.out.println("Coluna inválida. É 1, 2 ou 3!");
+            }
+            while((tentativa[1] < 1) || (tentativa[1] > 3));
+
+            tentativa[0]--;
+            tentativa[1]--;
+
+            if(!checaTentativa(tentativa, tabuleiro))
+                System.out.println("Esse local já foi marcado. Tente outro!");
+        }
+        while(!checaTentativa(tentativa, tabuleiro));
     }
 }
